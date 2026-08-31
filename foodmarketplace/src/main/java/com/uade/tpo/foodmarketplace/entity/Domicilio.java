e850fd36c0c1e2cae5dda4a740d0e6efb2934e20
NehuenZaloaga <nzaloaga@uade.edu.ar>
2026-08-31 08:34:54 -0300

    Update Domicilio.java

--- foodmarketplace/src/main/java/com/uade/tpo/foodmarketplace/entity/Domicilio.java
+++ foodmarketplace/src/main/java/com/uade/tpo/foodmarketplace/entity/Domicilio.java
@@ -2,7 +2,10 @@ package com.uade.tpo.foodmarketplace.entity;
 
 import org.hibernate.annotations.ValueGenerationType;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -11,8 +14,28 @@ import lombok.Data;
 
 public class Domicilio {
     @Id
-    @ValueGenerationType(stretegy = ValueGenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String calle;
+
+    @Column
+    private String numero;
+
+    @Column
+    private String ciudad;
+
+    @Column
+    private String provincia;
+
+    @Column
+    private String pais;
+
+    @Column
+    private String codigoPostal;
+
+
     
 
 }
